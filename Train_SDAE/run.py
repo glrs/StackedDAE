d96429d970b64a63633df8258528a8dee684c34b
Anastasios Glaros <glaros.t@gmail.com>
2016-04-08 16:54:55 +0200

    Cleaning up a bit for better readability

--- Train_SDAE/run.py
+++ Train_SDAE/run.py
@@ -28,6 +28,9 @@ from tensorflow.python.framework.errors import FailedPreconditionError
 pandas2ri.activate()
 numpy2ri.activate()
 r = robjects.r
+r_source = r['source']
+r_source('../Evaluation/evaluate_model.R', **{'print.eval': True})
+
 
 _data_dir = FLAGS.data_dir
 _output_dir = FLAGS.output_dir
@@ -81,12 +84,11 @@ def main():
     
     # Open DataFile
 
-    # Allan's Data
     start_time = time.time()
-    # datafile, (mapped_labels, label_map) = load_data('TPM', label_col=9, transpose=True)
-    # labelfile = load_data('Labels')
-    datafile, labels, (mapped_labels, label_map) = load_data('Linarsson', transpose=False)
-#     datafile, labels, (mapped_labels, label_map) = load_data(dataset='Allen', d_type='TPM', label_col=9, transpose=False)
+#     datafile, (mapped_labels, label_map) = load_data('TPM', label_col=9, transpose=True)
+#     labelfile = load_data('Labels')
+#     datafile, labels, (mapped_labels, label_map) = load_data('Linarsson', transpose=False)
+    datafile, labels, (mapped_labels, label_map) = load_data(dataset='Allen', d_type='TPM', label_col=7, transpose=False)
 
 #     print(mapped_labels, label_map)
 #     print(labels)
@@ -94,12 +96,10 @@ def main():
     print("Data Loaded. Duration:", time.time() - start_time)
     np.set_printoptions(threshold=np.nan)
 
-#     write_csv(pjoin(FLAGS.data_dir, "Labels_1.csv"), mapped_labels.tolist())
-
-#     a = Adasyn(datafile, mapped_labels, label_map[:,1], beta=0.5)
-#     datafile, mapped_labels = a.balance_all()
+    a = Adasyn(datafile, mapped_labels, label_map[:,1], beta=1)
+    datafile, mapped_labels = a.balance_all()
 #     a.save_data(pjoin(FLAGS.data_dir, 'TPM_balanced_data.csv'), pjoin(FLAGS.data_dir, 'Mapped_Labels_inOrder_balanced.csv'))
-#     del(a)
+    del(a)
 
     # Data Normalization
     start_time = time.time()
@@ -111,7 +111,7 @@ def main():
 #   mapped_labels, label_map = label_metadata(label_matrix=labelfile, label_col=7)
     num_classes = label_map.shape[0]
 
-            
+
     print("\nLabel Counts:")
     for i in xrange(num_classes):
         print("{: >30}\t".format(label_map[i,0]), len(all_indices(i, mapped_labels.tolist())))
@@ -120,30 +120,25 @@ def main():
     print("\nLoading train and test data-sets for pre-training")
     data = load_data_sets_pretraining(datafile_norm, split_only=False)
     print("\nTotal Number of Examples:", data.train.num_examples + data.test.num_examples)
-    
+
     nHLay = FLAGS.num_hidden_layers
     nHUnits = [getattr(FLAGS, "hidden{0}_units".format(j + 1)) for j in xrange(nHLay)]
-    
+
     # Get the number of existed features (e.g. genes) in the data-set 
     num_features = datafile_norm.shape[1]
+
     # Create the shape of the AutoEncoder
     sdae_shape = [num_features] + nHUnits + [num_classes]
     print(sdae_shape)
-    
-    
+
+    # Run pretraining step
     sdae = SDAE.pretrain_sdae(input_x=data, shape=sdae_shape)
     del(data)
-    
-#     print("Random Forest Before Finetuning")
-#     run_rf(datafile_norm, mapped_labels, sdae.get_weights, sdae.get_biases)
 
-    r_source = r['source']
-    r_source('../Evaluation/evaluate_model.R', **{'print.eval': True})
+    print("Random Forest Before Finetuning")
+    run_rf(datafile_norm, mapped_labels, sdae.get_weights, sdae.get_biases)
 
-    
-
-
-
+    # Create explanatory plots/graphs
     analyze(sdae, datafile_norm, labels)
 
         
@@ -151,37 +146,28 @@ def main():
 #         grdevices.pdf(pjoin(FLAGS.output_dir, pcafile))
 #         r.par(mfrow=r.c(1,2))
 #         p = r.prcomp(act)
-#         
 #         # btype : broad_type
 #         col = typeCols[btype[r.rownames(datafile)]]
 #         r.plot(p.rx2('x'), col=col, pch=20)
-#         r.plot(p.rx2('x')[:][2:3],col=col, pch=20)
-#         
+#         r.plot(p.rx2('x')[:][2:3],col=col, pch=20)  
 #         grdevices.dev_off()
-        
-#         plot_tSNE(act, mapped_labels, plot_name="tSNE_pre_layer_{}".format(layer.which))
-    
-        
+
+
     print("\nLoading train and test data-sets for Finetuning")
     data = load_data_sets(datafile_norm, mapped_labels)
     print("\nTotal Number of Examples:", data.train.num_examples + data.test.num_examples)
-    
-    sdae = SDAE.finetune_sdae(sdae=sdae, input_x=data, n_classes=num_classes, label_map=label_map[:,0]) #['broad_type']
-#     print("Random Forests After Finetuning for Autoencoder layers:")
-#     run_rf(datafile_norm, mapped_labels, sdae.get_weights, sdae.get_biases, n_layers=nHLay)
-#     print("Random Forests After Finetuning for all layers:")
-#     run_rf(datafile_norm, mapped_labels, sdae.get_weights, sdae.get_biases)
 
+    # Run finetuning step    
+    sdae = SDAE.finetune_sdae(sdae=sdae, input_x=data, n_classes=num_classes, label_map=label_map[:,0])
+
+    print("Random Forests After Finetuning for Autoencoder layers:")
+    run_rf(datafile_norm, mapped_labels, sdae.get_weights, sdae.get_biases, n_layers=nHLay)
+    print("Random Forests After Finetuning for all layers:")
+    run_rf(datafile_norm, mapped_labels, sdae.get_weights, sdae.get_biases)
+
+    # Create explanatory plots/graphs
     analyze(sdae, datafile_norm, labels)
 
-#     act = np.float32(datafile_norm)
-#     for layer in sdae.get_layers:
-#         fixed = False if layer.which > nHLay - 1 else True
-#         print("t-SNE for layer ", layer.which + 1)
-#         act = sdae.get_activation(act, layer.which, use_fixed=fixed)
-#         plot_tSNE(act, mapped_labels, plot_name="tSNE_layer_{}".format(layer.which))
-
-    
     print("\nConfiguration:")
     print("\n{: >45}\t".format("# Hidden Layers:"), nHLay)
     print("{: >45}\t".format("# Hidden Units:"), nHUnits)
@@ -205,19 +191,22 @@ def main():
 
 def analyze(sdae, datafile_norm, labels):
     def_colors = robjects.globalenv['def_colors']
+    do_analysis = robjects.globalenv['do_analysis']
+
     labels.reset_index(level=0, inplace=True)
     def_colors(labels)
-    do_analysis = robjects.globalenv['do_analysis']
     act = np.float32(datafile_norm)
+
     for layer in sdae.get_layers:
         fixed = False if layer.which > sdae.nHLayers - 1 else True
-#         if layer.which > nHLay - 1:
-#             break
+
         print("Analysis for layer {}:".format(layer.which + 1))
+
         try:
             act = sdae.get_activation(act, layer.which, use_fixed=fixed)
             temp = pd.DataFrame(data=act, index=labels[[0]]).reset_index(level=0)
             do_analysis(temp, pjoin(FLAGS.output_dir, "Layer_{}".format(layer.which)))
+            plot_tSNE(act, mapped_labels, plot_name="Pyhton_tSNE_layer_{}".format(layer.which))
         except FailedPreconditionError as e:
             break
 
